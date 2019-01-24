9412a7e10abb5fa521dc8f593ff0ffcbef6e5b52
KatoMono Enkeli <k4t0mono@gmail.com>
2019-01-24 17:01:05 -0200

    Log wrapper and help command

--- app.py
+++ app.py
@@ -1,21 +1,13 @@
 import os
 import logging
 from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
-from utils import get_logger
+from utils import get_logger, restricted, log
 from pprint import pprint
 from random import choice
 
-
+@log
 def echo_text(bot, update):
 	msg = update.message
-	logger = logging.getLogger('echo_text')
-
-	logger.info(
-		'echo_text from {}({})'.format(
-			msg.from_user.username,
-			msg.from_user.id
-		)
-	)
 
 	bot.send_message(
 		chat_id=msg.chat_id,
@@ -23,17 +15,9 @@ def echo_text(bot, update):
 	)
 
 
+@log
 def echo_sticker(bot, update):
 	msg = update.message
-	logger = logging.getLogger('echo_sticker')
-
-	logger.info(
-		'echo_sticker from {}({})'.format(
-			msg.from_user.username,
-			msg.from_user.id
-		)
-	)
-
 	ss = bot.get_sticker_set(msg.sticker.set_name)
 	s = choice(ss.stickers)
 
@@ -43,10 +27,45 @@ def echo_sticker(bot, update):
 	)
 
 
+@log
+@restricted
+def get_help(bot, update):
+	msg = update.message
+
+	txt = """
+Commands:
+
+*/new_wallet <name> [balance]*
+Add a new wallet to the list
+
+*/list_wallets*
+List the avaliable wallets
+
+*/set_default <name>*
+Set the default wallet for transactions
+
+*/get_default*
+Get the default wallet
+
+*/log*
+Get all transactions
+
+To add a new transaction just add send a message on the format:
+<desc> <value> [wallet]
+"""
+
+	bot.send_message(
+		chat_id=msg.chat_id,
+		parse_mode='Markdown',
+		text=txt
+	)
+
+
 if __name__ == '__main__':
 	updater = Updater(token=os.environ['QUARK_TOKEN'])
 	dispatcher = updater.dispatcher
 
+	dispatcher.add_handler(CommandHandler('help', get_help))
 	dispatcher.add_handler(MessageHandler(Filters.text, echo_text))
 	dispatcher.add_handler(MessageHandler(Filters.sticker , echo_sticker))
 
--- utils.py
+++ utils.py
@@ -1,10 +1,59 @@
 import logging
+from functools import wraps
+
+
+LIST_OF_ADMINS = [ 196044463 ]
 
 
 def get_logger(name, level=logging.INFO):
 	logger = logging.getLogger(name)
 	logging.basicConfig(
 		level=level,
-		format='[%(asctime)s] %(levelname)s - %(module)s.%(name)s - %(message)s'
+		format='[%(asctime)s] %(levelname)s - %(name)s - %(message)s'
 	)
 	return logger
+
+
+def log(func):
+	@wraps(func)
+	def wrapped(bot, update, *args, **kwargs):
+		logger = get_logger(func.__name__)
+		logger.info(
+			'{} from {}({})'.format(
+				func.__name__,
+				update.message.from_user.username,
+				update.message.from_user.id
+			)
+		)
+
+		return func(bot, update, *args, **kwargs)
+
+	return wrapped
+
+def restricted(func):
+	@wraps(func)
+	def wrapped(bot, update, *args, **kwargs):
+		logger = get_logger('restricted.{}'.format(func.__name__))
+		user_id = update.effective_user.id
+
+		if user_id not in LIST_OF_ADMINS:
+			bot.send_message(chat_id=update.message.chat_id, text='Unauthorized access denied.')
+			logger.warning(
+				"Unauthorized access denied for {}({})".format(
+					update.effective_user.username,
+					update.effective_user.id
+				)
+			)
+			return
+
+		return func(bot, update, *args, **kwargs)
+
+	return wrapped
+
+def send_multiple_messages(bot, chat_id, msgs=[]):
+	for msg in msgs:
+		bot.send_message(
+			chat_id=chat_id,
+			parse_mode='Markdown',
+			text=msg
+		)
